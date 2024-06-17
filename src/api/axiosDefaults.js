e685203b5e72d8f2657d2eb26e86bd00c010ac37
Rachael Bull <rachaelbull2021@gmail.com>
2024-06-17 17:08:08 +0000

    .

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://moment-f98bfaa6bcb4.herokuapp.com/'
+axios.defaults.baseURL = 'https://moment-api-728107cf0970.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
